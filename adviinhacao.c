8289e7a976d69065c040fa8843e4db6fdb10c452
LucasdoPradoTozzi <120129056+LucasdoPradoTozzi@users.noreply.github.com>
2023-08-22 17:26:22 -0300

    inicializando o jogo

---
+++ adviinhacao.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("****************************************\n");
+    printf("*Bem vindo ao nosso jogo de advinhação!*\n");
+    printf("****************************************\n");
+
+    return 0;
+}
