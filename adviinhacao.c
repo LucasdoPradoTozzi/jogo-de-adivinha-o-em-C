f26b53c5fc051192c58cd17ee31767568aac00e5
LucasdoPradoTozzi <120129056+LucasdoPradoTozzi@users.noreply.github.com>
2023-08-23 16:47:45 -0300

    adicionando o loop for ao jogo

--- adviinhacao.c
+++ adviinhacao.c
@@ -9,23 +9,28 @@ int main() {
 
     int chute;
 
-    printf("Qual o seu chute? ");
-    scanf("%d", &chute);
-    printf("Seu chute foi %d\n", chute);
+    for(int i = 1; i <= 3; i++) {
+        printf("Tentativa %d de 3\n", i);
+        printf("Qual o seu chute? ");
+        scanf("%d", &chute);
+        printf("Seu chute foi %d\n", chute);
 
-    if(chute == numerosecreto) {
-        printf("Parabéns, você acertou!\n");
-        printf("Jogue novamente, você é um bom jogador!\n");
-    }
-    else {
+        int acertou = chute == numerosecreto;
 
-        if(chute > numerosecreto) {
-            printf("Seu chute foi maior que o número secreto!\n");
+        if(acertou) {
+            printf("Parabéns, você acertou!\n");
+            printf("Jogue novamente, você é um bom jogador!\n");
         }
-
         else {
-            printf("Seu chute foi menor que o número secreto!\n");
+            int maior = chute > numerosecreto;
+            if(maior) {
+                printf("Seu chute foi maior que o número secreto!\n");
+            }
+
+            else {
+                printf("Seu chute foi menor que o número secreto!\n");
+            }
         }
-    }
-    
+    } 
+    printf("Fim de jogo!\n");
 }
