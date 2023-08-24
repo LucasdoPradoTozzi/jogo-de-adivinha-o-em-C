48e65ce296042b4b3bd382a03af6cda7bc8883f7
LucasdoPradoTozzi <120129056+LucasdoPradoTozzi@users.noreply.github.com>
2023-08-24 20:05:24 -0300

    tirando limite de tentativas adicionando while ao codigo

--- adviinhacao.c
+++ adviinhacao.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
-#define NUMERO_DE_TENTATIVAS 5
 
 int main() {
     printf("****************************************\n");
     printf("*Bem vindo ao nosso jogo de advinhação!*\n");
     printf("****************************************\n");
 
+    int tentativa = 1;
     int numerosecreto = 42;
 
     int chute;
 
-        for(int i = 1; i <= NUMERO_DE_TENTATIVAS; i++) {
-            printf("Tentativa %d de %d\n", i, NUMERO_DE_TENTATIVAS);
+        while(1) {
+            printf("Tentativa %d\n", tentativa);
             printf("Qual o seu chute? ");
             scanf("%d", &chute);
             printf("Seu chute foi %d\n", chute);
+
+            if(chute < 0) {
+                printf("Você não pode chutar números negativos!!\n");
+                continue;
+            }
 
             int acertou = chute == numerosecreto;
             int maior = chute > numerosecreto;
@@ -26,12 +31,15 @@ int main() {
             }
             else if(maior) {
                 printf("Seu chute foi maior que o número secreto!\n");
+                tentativa++;
             }
 
             else {
                 printf("Seu chute foi menor que o número secreto!\n");
+                tentativa++;
             }
         }
     
     printf("Fim de jogo!\n");
+    printf("Você acertou em %d tentativas!\n", tentativa);
 }
